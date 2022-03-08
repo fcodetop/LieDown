8eab1e153528d85eaca1b61a1b400a1957a06825
Rick <admin@fcode.top>
2022-03-08 16:31:22 +0800

    retry rpcnode

--- LieDown/Preload.cs
+++ LieDown/Preload.cs
@@ -13,36 +13,51 @@ namespace LieDown
 {
     public partial class Preload : Form
     {
+        private static readonly NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
         public Preload()
         {
             InitializeComponent();
         }
 
-        private  void Preload_Load(object sender, EventArgs e)
+        private async  void Preload_Load(object sender, EventArgs e)
         {
             var nodeList = NodeInfo.GetDefaultNodeList();
-            this.BeginInvoke(() => this.progressBar1.Value = 10);
-            Task.WhenAll(nodeList.Select(x => x.GetPreloadEndedAsync()))
-                 .ContinueWith(async (tasks) =>
-                 {
-                     this.BeginInvoke(() => this.progressBar1.Value = 50);
-                     var dialogResult = DialogResult.Cancel;
-                     var results = await tasks;
-                     this.BeginInvoke(() => this.progressBar1.Value = 99);
-                     await Task.Delay(300);
-                     if (results.Any(x => x))
+            bool retry = false;          
+            do
+            {
+                this.BeginInvoke(() => this.progressBar1.Value = 10);
+                await await Task.WhenAll(nodeList.Select(x => x.GetPreloadEndedAsync()))
+                     .ContinueWith(async (tasks) =>
                      {
-                         Program.Nodes = nodeList.ToList();
-                         dialogResult = DialogResult.OK;
-                     }
-                     else
-                     {
-                         MessageBox.Show("No valiable RPC node, application exit!");
-                     }
-                     this.Invoke(() => this.DialogResult = dialogResult);
 
-                 });
+                         this.BeginInvoke(() => this.progressBar1.Value = 50);
+                         var dialogResult = DialogResult.Cancel;
+                         var results = await tasks;
+                         this.BeginInvoke(() => this.progressBar1.Value = 99);
+                         await Task.Delay(300);
 
+                         if (results.Any(x => x))
+                         {
+                             Program.Nodes = nodeList.ToList();
+                             dialogResult = DialogResult.OK;
+                         }
+                         else
+                         {
+                             if (Program.PrivateKey != null) //delay and retry
+                             {
+                                 await Task.Delay(1000 * 60 * 5);
+                                 retry = true;
+                                 log.Warn("No valiable RPC node retry ...");
+                                 return;
+                             }
+                             MessageBox.Show("No valiable RPC node, application exit!");
+                         }
+                         retry = false;
+                        this.Invoke(() => this.DialogResult = dialogResult);
+
+                     });
+            }
+            while (retry);
 
         }
 
